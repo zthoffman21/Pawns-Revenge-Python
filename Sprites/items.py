2527b4f0ebb45b99701a222106e77a0d2d905729
Zachary Hoffman <128459283+zthoffman21@users.noreply.github.com>
2023-04-13 00:16:19 -0400

    Add files via upload

--- Sprites/items.py
+++ Sprites/items.py
@@ -1,8 +1,9 @@
 # Importing the libraries
 import pygame
-itemTextures = {"A":"Textures/A.png", "B":"Textures/B.png", "C":"Textures/C.png", "D":"Textures/D.png", "E":"Textures/E.png", "F":"Textures/F.png", "G":"Textures/G.png",
-                "H":"Textures/H.png", "1":"Textures/1.png", "2":"Textures/2.png", "3":"Textures/3.png", "4":"Textures/4.png", "5":"Textures/5.png", "6":"Textures/6.png", 
-                "7":"Textures/7.png", "8":"Textures/8.png"}
+itemTextures = {"A":"Textures/Items/A.png", "B":"Textures/Items/B.png", "C":"Textures/Items/C.png", "D":"Textures/Items/D.png", "E":"Textures/Items/E.png", 
+                "F":"Textures/Items/F.png", "G":"Textures/Items/G.png", "H":"Textures/Items/H.png", "1":"Textures/Items/1.png", "2":"Textures/Items/2.png", 
+                "3":"Textures/Items/3.png", "4":"Textures/Items/4.png", "5":"Textures/Items/5.png", "6":"Textures/Items/6.png", "7":"Textures/Items/7.png", 
+                "8":"Textures/Items/8.png"}
 
 class Items(pygame.sprite.Sprite):
     def __init__(self, type):
